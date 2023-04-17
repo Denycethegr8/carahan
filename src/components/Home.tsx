f19b405edcdf121545300c6be18660bf5b8e4a24
Denycethegr8 <jini3209@gmail.com>
2023-04-17 13:30:21 +0900

    add slider

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,41 +1,43 @@
-import { useState, useEffect, useRef } from "react";
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Home = () => {
-  const [imgSrc, setImgSrc] = useState("src/img/main1.png");
-  const [itemDesc, setItemDesc] = useState("MONEUF");
-  const countRef = useRef(0);
+  const sliderSettings = {
+    slidesToScroll: 1,
+    infinite: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  };
 
-  useEffect(() => {
-      setTimeout(() => {
-        switch (countRef.current) {
-          case 0:
-            setImgSrc("src/img/main1.png");
-            setItemDesc("MONEUF");
-            break;
-          case 1:
-            setImgSrc("src/img/main2.jpg");
-            setItemDesc("LUMI");
-            break;
-          case 2:
-            setImgSrc("src/img/main3.jpg");
-            setItemDesc("MARI STEIGER");
-            break;
-          default:
-            break;
-        }
-        
-        countRef.current += 1;
-      }, 1000);
-  }, [countRef, imgSrc, itemDesc]);
+  const imgBrand = [
+    {
+      imgSrc: "./src/img/main1.png",
+      name: "Moneuf",
+    },
+    {
+      imgSrc: "./src/img/main2.jpg",
+      name: "lUMI",
+    },
+    {
+      imgSrc: "./src/img/main3.jpg",
+      name: "MARI STEIGER",
+    },
+  ];
 
   return (
     <div className="home">
-      <article className="article">
-        <div className="main-img-div">
-          <img id="main-img" src={imgSrc}></img>
-        </div>
-        <p className="main-item-desc">{itemDesc}</p>
-      </article>
+      <Slider {...sliderSettings}>
+        {imgBrand.map((brand, index) => (
+          <article className="article" key={index}>
+            <div className="main-img-div">
+              <img id="main-img" src={brand.imgSrc}></img>
+            </div>
+            <p className="main-item-desc">{brand.name}</p>
+          </article>
+        ))}
+      </Slider>
     </div>
   );
 };
