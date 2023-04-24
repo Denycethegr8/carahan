c1ba05085aa6376d905f65dfc041d5af7b0df45d
Denycethegr8 <jini3209@gmail.com>
2023-04-24 13:36:51 +0900

    add gallery images

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 
 const Home = () => {
 
-  const homeGalleryImgsArr = ["../src/img/img-hubatt (2).jpg", "../src/img/img-hubatt (2).jpg", "../src/img/img-hubatt (2).jpg", "../src/img/img-hubatt (2).jpg","../src/img/img-hubatt (2).jpg","../src/img/img-hubatt (2).jpg"]
+  const homeGalleryImgsArr = ["../src/img/v25 (5).jpg", "../src/img/피네.jpg", "../src/img/프로피조리도구 (20).jpg", "../src/img/오일병 (6).jpg", "../src/img/주물 (8).jpg", "../src/img/저울&타이머 (3).jpg"]
 
   const homeGalleryImgs = homeGalleryImgsArr.map(img => <Link to="../Brands"><img className="home-gallery-img" src={img}/></Link>)
 
@@ -12,6 +12,9 @@ const Home = () => {
     <div className="home">
       <Slide />
       <div className="home-gallery">
+        <p>Styling Gallery</p>
+        <p></p>
+        <p></p>
         {homeGalleryImgs}
       </div>
       <Footer />
