4360e78052d70824c7eebe76d9037c7cd11cfcc6
Denycethegr8 <jini3209@gmail.com>
2023-04-25 13:38:36 +0900

    finish home styling gallery

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,18 +4,19 @@ import { Link } from "react-router-dom"
 
 const Home = () => {
 
-  const homeGalleryImgsArr = ["../src/img/v25 (5).jpg", "../src/img/피네.jpg", "../src/img/프로피조리도구 (20).jpg", "../src/img/오일병 (6).jpg", "../src/img/주물 (8).jpg", "../src/img/저울&타이머 (3).jpg"]
+  const homeGalleryImgsArr = ["../src/img/v25 (5).jpg", "../src/img/피네 (2).jpg", "../src/img/프로피조리도구 (20).jpg", "../src/img/오일병 (6).jpg", "../src/img/주물 (8).jpg", "../src/img/가죽매트 (2).jpg"]
 
   const homeGalleryImgs = homeGalleryImgsArr.map(img => <Link to="../Brands"><img className="home-gallery-img" src={img}/></Link>)
 
   return (
     <div className="home">
       <Slide />
-      <div className="home-gallery">
-        <p>Styling Gallery</p>
-        <p></p>
-        <p></p>
-        {homeGalleryImgs}
+      <div className="home-flex">
+        <p className="styling-gallery">Styling Gallery</p>
+        <div className="home-gallery">
+          {homeGalleryImgs}
+        </div>
+        <Link to="../Brands"><p className="view-more">VIEW MORE</p></Link>
       </div>
       <Footer />
     </div>
