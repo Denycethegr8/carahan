b34687be916612eed0e2f1e4cb62a761853c8050
Denycethegr8 <jini3209@gmail.com>
2023-05-03 13:30:19 +0900

    work on contact form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,12 +1,56 @@
-import Footer from "./Footer"
+import Footer from "./Footer";
 
 const Contact = () => {
-    return ( 
-        <div className="contact">
-            <div className="contact-div">sdfsd</div>
-            <Footer />
-        </div>
-     );
-}
- 
+  return (
+    <div className="contact">
+      <div className="contact-div">
+      <h2>Contact us</h2>
+        <tbody>
+            <tr>
+                <td>
+                    <p>Name</p>
+                </td>
+                <td>
+                    <input name="myEmail" type="text" />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <p>Email</p>
+                </td>
+                <td>
+                   <input name="myEmail" type="text" />
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <p>Phone</p>
+                </td>
+                <td>
+                    <input name="myPhone" type="text" />
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <p>Subject</p>
+                </td>
+                <td>
+                    <input name="mySubject" type="text" />
+                </td>
+            </tr>
+
+            <tr>
+                <td>
+                    <input type="submit" value="Send" />
+                </td>
+            </tr>
+        </tbody>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
 export default Contact;
