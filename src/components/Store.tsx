81b781316c2924a12a2fd77b3cf10a12de909513
Denycethegr8 <jini3209@gmail.com>
2023-05-09 12:46:02 +0900

    test

--- src/components/Store.tsx
+++ src/components/Store.tsx
@@ -6,11 +6,11 @@ const Store = () => {
             <div className="store-flex">
                 <div className="pricceclub">
                     <a className="store-imgs" target="_blank" href="https://priceclub.co.kr/"><img src="../src/img/priceclub.jpg" alt="pricceclub main image" /></a>
-                    <a target="_blank" href="https://priceclub.co.kr/"><p>프라이스클럽</p></a>
+                    <a target="_blank" className="store-name" href="https://priceclub.co.kr/"><p>프라이스클럽</p></a>
                 </div>
                 <div className="storefarm">
                     <a className="store-imgs" target="_blank" href="https://brand.naver.com/maristeiger"><img src="../src/img/storefarm.jpg" alt="naver storefarm main image" /></a>
-                    <a target="_blank" href="https://brand.naver.com/maristeiger"><p>NAVER 스토어팜</p></a>
+                    <a target="_blank" className="store-name" href="https://brand.naver.com/maristeiger"><p>NAVER 스토어팜</p></a>
                 </div>
             </div>
             <Footer />
